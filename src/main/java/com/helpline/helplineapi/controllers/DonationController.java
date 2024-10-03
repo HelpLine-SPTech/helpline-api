41e8e94be138ba0fe6f743a5c5c5083169eddaf5
Renan2805 <renansilva.dev@gmail.com>
2024-10-02 21:30:19 -0300

    add endpoint

--- src/main/java/com/helpline/helplineapi/controllers/DonationController.java
+++ src/main/java/com/helpline/helplineapi/controllers/DonationController.java
@@ -1,7 +1,10 @@
 package com.helpline.helplineapi.controllers;
 
+import com.helpline.helplineapi.data.contract.donations.confirm.ConfirmDonationRequest;
+import com.helpline.helplineapi.data.contract.donations.confirm.ConfirmDonationResponse;
 import com.helpline.helplineapi.data.contract.donations.report.GetDonationReportRequest;
 import com.helpline.helplineapi.entities.user.BaseUserEntity;
+import com.helpline.helplineapi.services.donation.ConfirmDonationService;
 import com.helpline.helplineapi.services.donation.DonationReportService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin
 @RestController
@@ -21,6 +25,9 @@ public class DonationController {
 
     @Autowired
     private DonationReportService donationReportService;
+
+    @Autowired
+    private ConfirmDonationService confirmDonationService;
 
     @GetMapping("/report")
     public ResponseEntity<FileSystemResource> donationsReport(@RequestAttribute("RequesterUser")BaseUserEntity requester) {
@@ -42,5 +49,14 @@ public class DonationController {
         httpHeaders.setContentDisposition(disposition);
 
         return new ResponseEntity<FileSystemResource>(response.getCsv(), httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/confirm/{id}")
+    public ResponseEntity<ConfirmDonationResponse> confirm(@PathVariable UUID id, @RequestAttribute("RequesterUser")BaseUserEntity requester) {
+        var request = new ConfirmDonationRequest();
+        request.setDonationId(id);
+        request.setRequesterUserId(requester.getId());
+
+        return confirmDonationService.process(request);
     }
 }
