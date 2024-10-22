87b950c19c2d4f9b91e397b5a74d16cb5f62788a
GuiScarabelli <guilherme.quadros@sptech.school>
2024-10-21 21:09:45 -0300

    Changing some dependencies warnings and adding the instance info controller

--- src/main/java/com/helpline/helplineapi/controllers/InstanceInfoController.java
+++ src/main/java/com/helpline/helplineapi/controllers/InstanceInfoController.java
@@ -1,5 +1,6 @@
 package com.helpline.helplineapi.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
